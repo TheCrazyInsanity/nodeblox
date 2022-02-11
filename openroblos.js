cf1f539cfe9eb86965dca9fd11063ab594df2098
TheCrazyInsanity <39921530+TheCrazyInsanity@users.noreply.github.com>
2022-02-11 18:44:18 -0500

    Removed some... vulgarity and useless comments
    hi

--- openroblos.js
+++ openroblos.js
@@ -13,7 +13,7 @@ const cookie = cookiehere
     console.log(`Preparing to join using ${currentUser.UserName} [${currentUser.UserID}]`)
     XCSRF = await noblox.getGeneralToken()
     //Get the xcsrf token using noblox, would have helped to know this earlier :/
-    console.log(`FUCK YEAH GOT THE FUCKING XC WHATEVER TOKEN POGG ${XCSRF}`);
+    console.log(`Got XCSRF token: ${XCSRF}`);
   }
   await xscrffuckin()
 
@@ -22,14 +22,14 @@ async function authticketfuckin(){
 let dumb = 1 
 //Axios post request to get an auth ticket
 dumb = axios({
-  method: 'POST', //you can set what request you want to be
+  method: 'POST',
   url: 'https://auth.roblox.com/v1/authentication-ticket',
   maxRedirects: 0,
   //Prevent redirects, useless but has no reason to be removed
   data: {},
   headers:{
       'x-csrf-token':XCSRF,
-      'referer':'https://www.roblox.com/', //Fuck you
+      'referer':'https://www.roblox.com/', //>:(
       Cookie: `.ROBLOSECURITY=${cookie}`
   }
 })
@@ -41,7 +41,7 @@ await authticketfuckin()
 //Axios get request to get the version number of roblox, so you dont have to update the script everytime you update roblox
 async function versionfuckin(){
 axios({
-  method: 'GET', //you can set what request you want to be
+  method: 'GET',
   url: 'https://s3.amazonaws.com/setup.roblox.com/version',
   maxRedirects: 0,
   //Prevent redirects, useless but has no reason to be removed
@@ -58,8 +58,7 @@ await versionfuckin()
 //Set variable time to current time in utc
 let time = new Date().getTime()
 
-//Get browser id (fuck you extra extra) 
-//Example link https://assetgame.roblox.com/game/PlaceLauncher.ashx?request=RequestGame&browserTrackerId=110592347077&placeId=843468296&isPlayTogetherGame=false
+//Get browser id ( >:( )
 var browseridlinksex = "Oops"
 async function urlfuckin(){
 browseridlinksex = `https://assetgame.roblox.com/game/PlaceLauncher.ashx?request=RequestGameJob^&browserTrackerId=${time}^&placeId=${gameid}^&gameId=${jobid}^&isPlayTogetherGame=false`
